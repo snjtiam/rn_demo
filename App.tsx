579be3945176e0affa56b36899b43180408dbbef
Sanjit <iamsnjt@gmail.com>
2023-09-12 21:27:44 +0530

    change src

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {View} from 'react-native';
-import RootStack from './srcAditya/Navigation/RootStack';
+import RootStack from './src/Navigation/RootStack';
 const App = () => {
   return (
     <View style={{flex: 1}}>
