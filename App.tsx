4b5e37765cf345aac141557a65246943f8efa08c
Sanjit <iamsnjt@gmail.com>
2023-09-12 21:28:55 +0530

    Added log

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {View} from 'react-native';
 import RootStack from './src/Navigation/RootStack';
 const App = () => {
+  console.log('THI IS LOG');
+
   return (
     <View style={{flex: 1}}>
       <RootStack />
