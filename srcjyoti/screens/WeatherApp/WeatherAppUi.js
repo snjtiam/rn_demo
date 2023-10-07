5a78dedd111c9760b883cc1970d36f35e7632c03
Sanjit <iamsnjt@gmail.com>
2023-10-07 21:00:19 +0530

    Added date fns

--- srcjyoti/screens/WeatherApp/WeatherAppUi.js
+++ srcjyoti/screens/WeatherApp/WeatherAppUi.js
@@ -7,56 +7,37 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 // import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Geolocation from '@react-native-community/geolocation';
 import WeatherApi_jyoti from '../../Apis/WeatherApi_jyoti';
+import {format} from 'date-fns';
 
 const WeatherAppUi = () => {
   const [location, setLocation] = React.useState(null);
   const [currentCondition, setCurrentCondition] = React.useState(null);
-  const [CurrentLocation, setCurrentLocation] = React.useState(null);
+  const [currentLocation, setCurrentLocation] = React.useState(null);
 
   const getGPSLocation = async () => {
     Geolocation.getCurrentPosition(info => {
       setLocation(info);
-      // console.log('jjjjjjjjjjjjjjj', location);
     });
   };
 
   const getGPSWeather = async () => {
     try {
       const {latitude, longitude} = location.coords;
-      const response = await WeatherApi_jyoti.getGPSWeather({lat: latitude, lon: longitude});
+      const currentLocation = await WeatherApi_jyoti.getGPSWeather({lat: latitude, lon: longitude});
+      setCurrentLocation(currentLocation);
 
-      const weatherResponse = await WeatherApi_jyoti.getCurrentWeather(response.Key);
+      const weatherResponse = await WeatherApi_jyoti.getCurrentWeather(currentLocation.Key);
       setCurrentCondition(weatherResponse);
-      // console.log('uuuuuuuuuuuuuuuuuuuuu', response)
-
-      const weatherLocation = async () => { await WeatherApi_jyoti.getCurrentLocation(response.Key)};
-      setCurrentLocation(weatherLocation);
-
-    console.log('RESPONSE', response, '----', JSON.stringify(weatherResponse));
-    } catch (error) {}
+    } catch (error) {
+      console.log('API ERR', error);
+    }
   };
-
-  // const getGPSWeather = async () => {
-  //   try {
-  //     const {latitude, longitude} = location.coords;
-  //     const response = await WeatherApi_jyoti.getGPSWeather({lat: latitude, lon: longitude});
-  
-  //     const weatherResponse = await WeatherApi_jyoti.getCurrentWeather(response.Key);
-  //     setCurrentCondition(weatherResponse);
-  
-  //     const locationResponse = await WeatherApi_jyoti.getCurrentLocation(response.Key);
-  //     setCurrentLocation(locationResponse);
-  
-  //     console.log('RESPONSE', response, '----', JSON.stringify(weatherResponse));
-  //   } catch (error) {
-  //     // Handle errors here
-  //   }
-  // };
-  
 
   const temperature = currentCondition ? currentCondition[0]?.Temperature?.Metric?.Value ?? '--' : '--';
   const condition = currentCondition ? currentCondition[0]?.WeatherText ?? '--' : '--';
-  const countrytitle = CurrentLocation ? CurrentLocation[0]?.EnglishName ?? '--' : '--';
+  const countrytitle = currentLocation ? currentLocation?.EnglishName ?? '--' : '--';
+  const locationDate = currentCondition ? currentCondition[0]?.LocalObservationDateTime ?? new Date() : new Date();
+  const date = format(new Date(locationDate), 'EEEE - dd MMM');
 
   React.useEffect(() => {
     if (location !== null) {
@@ -65,17 +46,18 @@ const WeatherAppUi = () => {
   }, [location]);
 
   React.useEffect(() => {
+    // Access storage
+    //Has data--> Load from storage
+    //No data--> Load from API
     getGPSLocation();
   }, []);
-
-  // DateFNS.format(new Date('2023-10-04T08:43:00-07:00'), 'CCCC - MMM');
 
   return (
     <View style={{flex: 1, backgroundColor: '#fff1'}}>
       <View style={{flexDirection: 'row', justifyContent: 'space-between', marginVertical: 20, marginHorizontal: 25}}>
         <Entypo name="menu" size={22} color="black" style={{alignSelf: 'center'}} />
         <View>
-          <Text style={{fontSize: 17, color: 'black', fontWeight: '800'}}>{countrytitle}Los Angeles, USA</Text>
+          <Text style={{fontSize: 17, color: 'black', fontWeight: '800'}}>{countrytitle}</Text>
           <Text style={{fontSize: 13, color: 'black', fontWeight: '400', textAlign: 'center'}}>Today, 08:16</Text>
         </View>
         <Entypo name="plus" size={22} color="black" style={{alignSelf: 'center'}} />
@@ -94,16 +76,13 @@ const WeatherAppUi = () => {
             <Text style={{fontSize: 15, color: 'black', fontWeight: 'bold', textAlign: 'center'}}>Sunny Day</Text>
           </View>
         </View>
-        <Text style={{fontSize: 25, color: 'black', fontWeight: '500', marginHorizontal: 25, marginTop: -4}}>
-          Saturday <Fontisto name="minus-a" /> 10 Feb
-        </Text>
+        <Text style={{fontSize: 25, color: 'black', fontWeight: '500', marginHorizontal: 25, marginTop: -4}}>{date}</Text>
         <View style={{flexDirection: 'row'}}>
-          <Text style={{fontSize: 140, color: 'black', fontWeight: 'bold', marginLeft: 25}}>{temperature+ '\u00b0'}</Text>
+          <Text style={{fontSize: 140, color: 'black', fontWeight: 'bold', marginLeft: 25}}>{temperature + '\u00b0'}</Text>
           {/* <FontAwesome name="circle-o" size={60} style={{fontWeight: 'bold', justifyContent: 'flex-start', alignSelf: 'flex-start'}} /> */}
         </View>
         <Text style={{fontSize: 40, color: 'black', fontWeight: 'bold', marginLeft: 25}}>{condition}</Text>
       </View>
-
     </View>
   );
 };
