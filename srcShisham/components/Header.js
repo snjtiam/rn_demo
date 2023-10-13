286aa07dfcce927c69c7175be514be4a1c16c2fc
Sanjit <mail@mail.com>
2023-10-13 20:36:54 +0545

    changed header country

--- srcShisham/components/Header.js
+++ srcShisham/components/Header.js
@@ -3,12 +3,49 @@ import React from 'react'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import WeatherUI from '../screens/WeatherUI'
+import {format} from 'date-fns';
+import WeatherApis from '../Apis/WeatherApis';
+import Geolocation from '@react-native-community/geolocation'
 const Header = () => {
+    const [location, setLocation] = React.useState(null);
+    const [currentCondition, setCurrentCondition] = React.useState(null);
+    const [currentLocation, setCurrentLocation] = React.useState(null);
+  
+    const getLocation =async()=>{
+      Geolocation.getCurrentPosition(info => {
+        setLocation(info);
+      });
+    }
+    const getWeather = async() => {
+      try {
+        const {latitude, longitude} = location.coords;
+        const currentLocation = await WeatherApis.getWeather({lat: latitude, lon: longitude});
+        setCurrentLocation(currentLocation);
+  
+        const weatherResponse = await WeatherApis.getCurrentWeather(currentLocation.Key);
+        setCurrentCondition(weatherResponse);
+      } catch (error) {
+        console.log('Error', error);
+      }
+    };
+    const countrytitle = currentLocation ? currentLocation?.EnglishName ?? '--' : '--';
+    const locationDate = currentCondition ? currentCondition[0]?.LocalObservationDateTime ?? new Date() : new Date();
+    const time = format(new Date(locationDate), 'hh : mm')
+    console.log('Header:', JSON.stringify(currentCondition));
+
+  React.useEffect(() => {
+    if (location !== null) {
+      getWeather();
+    }
+  }, [location]);
+
+  React.useEffect(() => {
+    getLocation();
+  }, []);
   return (
-    
     <View style={styles.container}>
         <Ionicons name='menu-outline' style={styles.menu} />
-    <View ><Text style={styles.Text1}>Los angelos ,USA</Text></View>
+    <View ><Text style={styles.Text1}>{countrytitle}</Text></View>
     <Text style={styles.Text2}>Today 06:36</Text>
     <Icon name='plus' 
     style={styles.icon} />
